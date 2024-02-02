9c57d2d14e350be4b36474972ca24ea515f12e04
thomaskywong <thomaskywong@yahoo.com>
2024-02-02 20:25:06 +0800

    UserCommentDTO revised

--- bootcamp-sb-forum2/src/main/java/com/vtxlab/bootcamp/bootcampsbforum2/controller/impl/CommentController.java
+++ bootcamp-sb-forum2/src/main/java/com/vtxlab/bootcamp/bootcampsbforum2/controller/impl/CommentController.java
@@ -2,20 +2,19 @@ package com.vtxlab.bootcamp.bootcampsbforum2.controller.impl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.vtxlab.bootcamp.bootcampsbforum2.controller.CommentOperation;
 import com.vtxlab.bootcamp.bootcampsbforum2.dto.gov.CommentDTO;
 import com.vtxlab.bootcamp.bootcampsbforum2.dto.gov.UserCommentDTO;
-import com.vtxlab.bootcamp.bootcampsbforum2.dto.gov.mapper.GovMapper;
 import com.vtxlab.bootcamp.bootcampsbforum2.infra.ApiResp;
 import com.vtxlab.bootcamp.bootcampsbforum2.infra.ResourceNotFound;
 import com.vtxlab.bootcamp.bootcampsbforum2.infra.Syscode;
-import com.vtxlab.bootcamp.bootcampsbforum2.model.dto.jdh.Comment;
+import com.vtxlab.bootcamp.bootcampsbforum2.model.dto.jdh.User;
 import com.vtxlab.bootcamp.bootcampsbforum2.service.CommentService;
 import com.vtxlab.bootcamp.bootcampsbforum2.service.UserService;
 
@@ -41,28 +40,69 @@ public class CommentController implements CommentOperation {
   public ResponseEntity<ApiResp<UserCommentDTO>> getUserComments(int userId)
       throws ResourceNotFound {
 
-    // Get List<comment> related to userId, wrap into Optional
-
-    Optional<UserCommentDTO> userCommentDTO = userService.getUsers() //
-        .stream() //
-        .filter(e -> e.getId() == userId) //
-        .map(e -> {
-          List<Comment> comments = commentService.getComments();
-          return GovMapper.map(e, comments);
-        }) //
-        .findFirst();
     
-    // Check if Optional isPresent, if true then wrap UserCommentDTO to ApiResp
-    if (userCommentDTO.isPresent()) {
-      ApiResp<UserCommentDTO> apiResp = ApiResp.<UserCommentDTO>builder() //
-                                               .code(Syscode.OK.getCode()) //
-                                               .message(Syscode.OK.getMessage()) //
-                                               .data(userCommentDTO.get()) //
-                                               .build();
-      return ResponseEntity.ok(apiResp);
+    Optional<User> userOp= userService.getUsers().stream() //
+                                .filter( e -> e.getId() == userId) //
+                                .findFirst();
+
+    if (userOp.isEmpty()) {
+      throw new ResourceNotFound(Syscode.NOTFOUND);
     }
 
-    throw new ResourceNotFound(Syscode.NOTFOUND);
+    User user = userOp.get();
+
+    List<CommentDTO> commentDTOs = commentService.getComments().stream() //
+                                      .filter(e -> e.getPostId() == userId) //
+                                      .map( e -> {
+                                            return CommentDTO.builder() //
+                                                      .postId(e.getPostId()) //
+                                                      .name(e.getName()) //
+                                                      .email(e.getEmail()) //
+                                                      .body(e.getBody()) //
+                                                      .build(); //
+                                            }) //
+                                      .collect(Collectors.toList());
+    
+    UserCommentDTO userCommentDTO = UserCommentDTO.builder() //
+                                                  .id(user.getId()) //
+                                                  .username(user.getUsername()) //
+                                                  .email(user.getEmail()) //
+                                                  .phone(user.getPhone()) //
+                                                  .commentDTOs(commentDTOs) //
+                                                  .build(); //
+    
+    ApiResp<UserCommentDTO> apiResp = ApiResp.<UserCommentDTO>builder() //
+                                             .code(Syscode.OK.getCode()) //
+                                             .message(Syscode.OK.getMessage()) //
+                                             .data(userCommentDTO) //
+                                             .build();
+   
+    return ResponseEntity.ok(apiResp);
+
+
+    // Get List<comment> related to userId, wrap into Optional
+
+
+    // Optional<UserCommentDTO> userCommentDTO = userService.getUsers() //
+    //     .stream() //
+    //     .filter(e -> e.getId() == userId) //
+    //     .map(e -> {
+    //       List<Comment> comments = commentService.getComments();
+    //       return GovMapper.map(e, comments);
+    //     }) //
+    //     .findFirst();
+    
+    // // Check if Optional isPresent, if true then wrap UserCommentDTO to ApiResp
+    // if (userCommentDTO.isPresent()) {
+    //   ApiResp<UserCommentDTO> apiResp = ApiResp.<UserCommentDTO>builder() //
+    //                                            .code(Syscode.OK.getCode()) //
+    //                                            .message(Syscode.OK.getMessage()) //
+    //                                            .data(userCommentDTO.get()) //
+    //                                            .build();
+    //   return ResponseEntity.ok(apiResp);
+    // }
+
+    // throw new ResourceNotFound(Syscode.NOTFOUND);
 
   }
 
